882151366914c2d6afe86afc0418086b603d782f
richelbilderbeek <richel@richelbilderbeek.nl>
2020-08-11 11:58:57 +0200

    Moved testing proteomes to bbbq

--- create_haplotypes_lut.R
+++ create_haplotypes_lut.R
@@ -1,13 +1,21 @@
-# Creates the 'haplotypes.csv' file, which maps a haplotype to its ID
+# Creates the 'haplotypes_lut.csv' file, which maps a haplotype to its ID
 #
 # Usage:
 #
 #   Rscript create_haplotypes.R
+#
+# Usage, for only 4 haplotypes:
+#
+#   Rscript create_haplotypes.R test
 
 target_filename <- "haplotypes_lut.csv"
 message("'target_filename': ", target_filename)
 
 t <- bbbq::create_haplotypes_lut()
+
+if (length(args) == 1 && args[1] == "test") {
+  t <- t[c(1, 2, 20, 21), ]
+}
 
 readr::write_csv(t, target_filename)
 
---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(bbbq1)
+
+test_check("bbbq1")
