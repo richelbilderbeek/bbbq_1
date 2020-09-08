3a86c0e6b55e0066e89ada58c83e1d2184319eb9
richelbilderbeek <richel@richelbilderbeek.nl>
2020-09-08 13:27:18 +0200

    Use 100 GB (from 10 GB), progres https://github.com/richelbilderbeek/bbbq_article/issues/79

--- run_long_hard_r_script.sh
+++ run_long_hard_r_script.sh
@@ -9,7 +9,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --ntasks=1
-#SBATCH --mem=10G
+#SBATCH --mem=100G
 #SBATCH --job-name=run_long_hard_r_script
 #SBATCH --output=run_long_hard_r_script_%j.log
 module load R
